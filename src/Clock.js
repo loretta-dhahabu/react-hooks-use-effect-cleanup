a2162f215361b8af2f946650c1a8b47e5c3c9613
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-17 14:16:24 +0300

    Done.

--- src/Clock.js
+++ src/Clock.js
@@ -3,11 +3,18 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
-    setInterval(() => {
+  useEffect( () =>
+  {
+    const timerID = setInterval(() => {
       setTime(new Date());
-    }, 1000);
-  }, []);
+    }, 1000 );
+    // returning a cleanup function
+    return function cleanup ()
+    {
+      clearInterval( timerID );
+    };
+  }, [] );
+  
 
   return <div>{time.toString()}</div>;
 }
